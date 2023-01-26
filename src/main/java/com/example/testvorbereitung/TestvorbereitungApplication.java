e133f5ab66ee2607cd350058fc8fa0b26e93d0d5
Jordi Rieder <e12123463@student.tuwien.ac.at>
2023-01-26 12:26:10 +0100

    Initial commit

---
+++ src/main/java/com/example/testvorbereitung/TestvorbereitungApplication.java
@@ -0,0 +1,13 @@
+package com.example.testvorbereitung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestvorbereitungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestvorbereitungApplication.class, args);
+    }
+
+}
